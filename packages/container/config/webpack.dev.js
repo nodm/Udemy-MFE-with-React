904ed839a08f32313fe11f71c4db0569cb97fdb6
Dmytro Novikov <nodm.pe@gmail.com>
2023-07-02 22:45:38 +0300

    Add deployment setup

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJSON = require('../package.json');
 const commonConfig = require('./webpack.common');
@@ -18,10 +17,7 @@ const devConfig = {
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
-      shared: packageJSON.dependenciess,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+      shared: packageJSON.dependencies,
     }),
   ],
 };
