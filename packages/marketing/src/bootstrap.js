3f7b65ea9b9d5973c3bab279a7c25d6660e1df08
Dmytro Novikov <nodm.pe@gmail.com>
2024-01-05 15:06:47 +0200

    V098: Implement Multi-Tier Navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
+import { createMemoryHistory,createBrowserHistory } from 'history';
 import { App } from './App';
 
-export const mount = (containerElement) => {
-  ReactDOM.render(<App />, containerElement);
+export const mount = (containerElement, { onNavigate, history = createMemoryHistory() }) => {
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, containerElement);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const appRoot =  document.getElementById('marketing-dev-root');
 
   if (appRoot) {
-    mount(appRoot);
+    mount(appRoot, { history: createBrowserHistory() });
   }
 }
